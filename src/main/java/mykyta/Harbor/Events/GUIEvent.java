659c85016910d6f53a1ffc029dac17e64d6d820d
BuildTools <nkomarn@hotmail.com>
2019-05-08 07:22:22 -0700

    GUIEvent Merge

--- src/main/java/mykyta/Harbor/Events/GUIEvent.java
+++ src/main/java/mykyta/Harbor/Events/GUIEvent.java
@@ -12,12 +12,8 @@ public class GUIEvent implements Listener {
     @EventHandler
     public void onClick(InventoryClickEvent event) {
         if (event.getInventory().getHolder() != null && event.getInventory().getHolder() instanceof Holder) {
-<<<<<<< HEAD
-            GUIType t = ((Holder) event.getInventory().getHolder()).getType();
-=======
             Player p = ((Player) event.getWhoClicked());
             GUIType t = ((GUIHolder) event.getInventory().getHolder()).getType();
->>>>>>> 0f80c831f481f11d5588813b6391b9162173782a
             if (t.equals(GUIType.SLEEPING)) event.setCancelled(true);
         }
     }
@@ -25,12 +21,8 @@ public class GUIEvent implements Listener {
     @EventHandler
     public void onDrag(InventoryDragEvent event) {
         if (event.getInventory().getHolder() != null && event.getInventory().getHolder() instanceof Holder) {
-<<<<<<< HEAD
-            GUIType t = ((Holder) event.getInventory().getHolder()).getType();
-=======
             Player p = ((Player) event.getWhoClicked());
             GUIType t = ((GUIHolder) event.getInventory().getHolder()).getType();
->>>>>>> 0f80c831f481f11d5588813b6391b9162173782a
             if (t.equals(GUIType.SLEEPING)) event.setCancelled(true);
         }
     }

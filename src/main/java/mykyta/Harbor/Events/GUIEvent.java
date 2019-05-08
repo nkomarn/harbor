0f80c831f481f11d5588813b6391b9162173782a
Mykyta <nkomarn@hotmail.com>
2019-05-08 06:58:23 -0700

    Move GUI-related items to GUI package

--- src/main/java/mykyta/Harbor/Events/GUIEvent.java
+++ src/main/java/mykyta/Harbor/Events/GUIEvent.java
@@ -6,15 +6,15 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryDragEvent;
 
-import mykyta.Harbor.GUIType;
-import mykyta.Harbor.Holder;
+import mykyta.Harbor.GUI.GUIType;
+import mykyta.Harbor.GUI.GUIHolder;
 
 public class GUIEvent implements Listener {
     @EventHandler
     public void onClick(InventoryClickEvent event) {
         if (event.getInventory().getHolder() != null && event.getInventory().getHolder() instanceof Holder) {
             Player p = ((Player) event.getWhoClicked());
-            GUIType t = ((Holder) event.getInventory().getHolder()).getType();
+            GUIType t = ((GUIHolder) event.getInventory().getHolder()).getType();
             if (t.equals(GUIType.SLEEPING)) event.setCancelled(true);
         }
     }
@@ -23,7 +23,7 @@ public class GUIEvent implements Listener {
     public void onDrag(InventoryDragEvent event) {
         if (event.getInventory().getHolder() != null && event.getInventory().getHolder() instanceof Holder) {
             Player p = ((Player) event.getWhoClicked());
-            GUIType t = ((Holder) event.getInventory().getHolder()).getType();
+            GUIType t = ((GUIHolder) event.getInventory().getHolder()).getType();
             if (t.equals(GUIType.SLEEPING)) event.setCancelled(true);
         }
     }
